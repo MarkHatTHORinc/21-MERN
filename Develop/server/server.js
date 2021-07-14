b8b5b9596c30ad55e1b1594b7adaeb4e6d3a5690
MarkHatTHORinc <markh@thorinc.com>
2021-07-13 22:04:51 -0400

    Add Schema

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -25,12 +25,9 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`🌍 Now listening on localhost:${PORT}`);
-//     // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-//   });
-// });
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
